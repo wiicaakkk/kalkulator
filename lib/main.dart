b8552722f70fe32bf329c8ce3981b6053eec75e3
wiicaakkk <bagus.wicaksono285@gmail.com>
2022-07-25 15:54:04 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:calculator/calculator.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(Myapp());
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Calculator(),
+    );
+  }
+}
